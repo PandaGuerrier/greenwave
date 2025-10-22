d2a8954f55eceaa04173f8add4247cbb05efac73
pandaguerrier <juleslofficial@gmail.com>
2025-10-22 10:32:44 +0200

    feat: added items to buy, finished dynamic sub

---
+++ app/users/database/migrations/1761120590553_create_items_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'items'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('stripe_id').notNullable().unique()
+      table.string('name').notNullable()
+      table.text('description').notNullable()
+      table.integer('price_cents').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
